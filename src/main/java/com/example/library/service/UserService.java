0b433982a6a3c22df89d948f54df677beca9b073
William <williamalfredo85@gmail.com>
2020-09-30 11:49:52 +0700

    fix user service

--- src/main/java/com/example/library/service/UserService.java
+++ src/main/java/com/example/library/service/UserService.java
@@ -1,12 +1,19 @@
 package com.example.library.service;
 
+import com.example.library.dto.UserDTO;
 import com.example.library.entity.User;
+import com.example.library.exception.BaseErrorException;
+import com.example.library.exception.ErrorMessage;
+import com.example.library.mapper.UserMapper;
 import com.example.library.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -15,16 +22,56 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> findAllUser(){
-        return userRepository.findAll();
+    @Autowired
+    private UserMapper userMapper;
+
+    public List<UserDTO> findAllUser() {
+        return userRepository.findAllByDeletedAtIsNull().stream().map(u -> {
+            return new UserDTO()
+                    .setId(u.getId())
+                    .setName(u.getName())
+                    .setEmail(u.getEmail())
+                    .setCreatedAt(u.getCreatedAt().toString())
+                    .setUpdatedAt(u.getUpdatedAt().toString());
+        }).collect(Collectors.toList());
     }
 
-    public User getUser(Long id){
-        User user = userRepository.findById(id).get();
-        return user;
+    public UserDTO getUser(Long id) {
+        User user = userRepository.findByIdAndDeletedAtIsNull(id)
+                .orElseThrow(() -> new BaseErrorException(HttpStatus.NOT_FOUND,
+                        ErrorMessage.USER_NOT_FOUND.getMessage()));
+
+        return convertToDto(user);
     }
 
-    public User create(User request){
-        return userRepository.save(request);
+    public UserDTO create(UserDTO request) {
+        User user = convertToEntity(request);
+        return convertToDto(userRepository.save(user));
+    }
+
+    public UserDTO update(Long id, UserDTO request) {
+        User user = userRepository.findByIdAndDeletedAtIsNull(id)
+                .orElseThrow(() -> new BaseErrorException(HttpStatus.NOT_FOUND,
+                        ErrorMessage.USER_NOT_FOUND.getMessage()));
+
+        User updatedUser = user.setEmail(request.getEmail())
+                .setName(request.getName());
+
+        // update
+        return convertToDto(userRepository.save(updatedUser));
+    }
+
+    /*
+    *
+    * Private methods
+    *
+    * */
+
+    private UserDTO convertToDto(User user){
+        return userMapper.userToUserDTO(user);
+    }
+
+    private User convertToEntity(UserDTO userDTO){
+        return userMapper.userDtoToUser(userDTO);
     }
 }
