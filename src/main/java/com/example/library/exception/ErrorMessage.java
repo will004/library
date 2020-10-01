4202987fb6734e315cf7641e2407efee180631ac
William <williamalfredo85@gmail.com>
2020-10-01 15:41:13 +0700

    add new error message

--- src/main/java/com/example/library/exception/ErrorMessage.java
+++ src/main/java/com/example/library/exception/ErrorMessage.java
@@ -4,6 +4,7 @@ public enum ErrorMessage {
     USER_NOT_FOUND("Username does not exist"),
     BOOK_NOT_FOUND("Book does not exist"),
     BOOK_OUT_OF_STOCK("Book is out of stock"),
+    TRANSACTION_NOT_FOUND("Transaction not found"),
     ALREADY_BORROW_BOOK("User already borrow the book(s)");
 
     private String message;
