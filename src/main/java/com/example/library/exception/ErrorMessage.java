63db253831f9dbd827bcd26691e1e8a77b503af4
William <williamalfredo85@gmail.com>
2020-10-01 14:09:36 +0700

    add error message

--- src/main/java/com/example/library/exception/ErrorMessage.java
+++ src/main/java/com/example/library/exception/ErrorMessage.java
@@ -2,7 +2,9 @@ package com.example.library.exception;
 
 public enum ErrorMessage {
     USER_NOT_FOUND("Username does not exist"),
-    BOOK_NOT_FOUND("Book does not exist");
+    BOOK_NOT_FOUND("Book does not exist"),
+    BOOK_OUT_OF_STOCK("Book is out of stock"),
+    ALREADY_BORROW_BOOK("User already borrow the book(s)");
 
     private String message;
 
