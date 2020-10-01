eef46dfb8f2f183c22e6d6a248a59a060e410d70
William <williamalfredo85@gmail.com>
2020-10-01 15:45:21 +0700

    tidying up code

--- src/main/java/com/example/library/repository/TransactionDetailRepository.java
+++ src/main/java/com/example/library/repository/TransactionDetailRepository.java
@@ -4,9 +4,6 @@ import com.example.library.entity.TransactionDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface TransactionDetailRepository extends JpaRepository<TransactionDetail, Long> {
 }
